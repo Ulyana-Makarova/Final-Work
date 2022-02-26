fb627a825f6ef8a881adb746a7a36abb81886d64
ulyana <ulyana@email.com>
2022-02-27 01:28:50 +0900

    Addead Task

---
+++ Task/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] array = { "103", "F", "Hello", ":-)" };
+Result(array);
+void Result(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) Console.Write($"{array[i]} ");
+    }
+    Console.Write(" ");
+}
