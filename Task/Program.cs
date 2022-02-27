409675efdf4f4a844d5f10454a8e5c5917d6e985
ulyana <ulyana@email.com>
2022-02-27 12:13:50 +0900

    Second save file task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,10 +1,10 @@
-﻿string[] array = { "103", "F", "Hello", ":-)" };
+﻿string[] array = { "103", "Fljg", "Hello", ":-)" };
 Result(array);
 void Result(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3) Console.Write($"{array[i]} ");
+        if (array[i].Length <= 3) Console.WriteLine($"{array[i]} ");
     }
     Console.Write(" ");
 }
