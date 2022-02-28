b0b28c8c1f22c2ef497db8665b5b49ab8634f249
ulyana <ulyana@email.com>
2022-02-28 22:28:56 +0900

    Added T2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -12,12 +12,12 @@ string[] newarray(string[] array)
     }
 
     string[] newarray = new string[count];
-    for (int i = 0, j = 0; i < array.Length; i++)
+    for (int i = 0, b = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            newarray[j] = array[i];
-            j++;
+            newarray[b] = array[i];
+            b++;
         }
     }
     return newarray;
